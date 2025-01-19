d398a59657529b74cc21c7389e005391b0fe9335
Brian Hrenak <bhrenak@gmail.com>
2025-01-19 05:44:40 -0500

    cleanup

--- src/js/theme.js
+++ src/js/theme.js
@@ -5,17 +5,19 @@ class Theme {
   }
 
   setTheme() {
-    if (this.theme === 'light') {
-      localStorage.setItem('ctv-theme','light');
-      document.body.classList.remove('theme-dark');
-    } else {
+    if (this.theme === 'dark') {
       localStorage.setItem('ctv-theme','dark');
       document.body.classList.add('theme-dark');
+      document.body.classList.remove('theme-light');
+    } else {
+      localStorage.setItem('ctv-theme','light');
+      document.body.classList.add('theme-light');
+      document.body.classList.remove('theme-dark');
     }
   }
 
   toggleTheme() {
-    this.theme = (this.theme === 'light') ? 'dark' : 'light';
+    this.theme = (this.theme === 'dark') ? 'light' : 'dark';
     this.setTheme();
   }
   
